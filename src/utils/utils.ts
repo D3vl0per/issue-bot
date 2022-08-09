e26934a41634ced341107a9fa30f732777c12098
0xAndrewBlack <lemon@zgen.hu>
2022-08-09 14:09:49 +0200

    ⚡️ feat: Updated commands and db functions + utility stuff.

---
+++ src/utils/utils.ts
@@ -0,0 +1,26 @@
+export function stripStatusFromThread(threadName: string): string {
+	return threadName
+		.split(' ')
+		.reverse()
+		.join(' ')
+		.substring(0, threadName.length - 4)
+		.split(' ')
+		.reverse()
+		.join(' ');
+}
+
+export const labels = [
+	{ label: 'Backlog', value: 'backlog' },
+	{ label: 'Todo', value: 'todo' },
+	{ label: 'In-Progress', value: 'wip' },
+	{ label: 'Testing', value: 'testing' },
+	{ label: 'Done', value: 'done' },
+];
+
+export const labelsWithEmojis = [
+	{ label: 'Backlog', value: 'backlog', emoji: '📁' },
+	{ label: 'Todo', value: 'todo', emoji: '📝' },
+	{ label: 'In-Progress', value: 'wip', emoji: '🚧' },
+	{ label: 'Testing', value: 'testing', emoji: '🧪' },
+	{ label: 'Done', value: 'done', emoji: '✅' },
+];
